fc44ca145ae65d0f8e3954b4fda9767e74dbde43
nitesh <nitsmuchhal@gmail.com>
2024-11-18 23:41:55 +0530

    Testimonial section added

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sectionhead from './Sectionhead'
+import { ImQuotesLeft } from "react-icons/im";
+import Card from '../UI/Card'
+import { IoIosArrowDroprightCircle ,IoIosArrowDropleftCircle} from "react-icons/io";
+import { testimonials } from '../Data';
 
 function Testimonials() {
+    const[index,setIndex] = useState(0)
+    const {name,quote,job,avatar} = testimonials[index];
   return (
-    <div>Testimonials</div>
+    <section className="testimonials">
+        <div className="container testimonials_container">
+            <Sectionhead icon={<ImQuotesLeft/>} title="Testimonials"/>
+            <Card className="testimonial">
+                  <div className="tesimonial_avatar">
+                    <img src={avatar} alt="" />
+                  </div>
+                  <p className='testimonial_quote'>
+                    {`"${quote}`}
+                  </p>
+                  <h5>{name}</h5>
+                  <small className="testimonial_title">{job}</small>
+            </Card>
+        </div>
+    </section>
   )
 }
 
