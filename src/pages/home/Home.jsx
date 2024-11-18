6da924375260a1538b74ceadbbd5ef0442ab2707
nitesh <nitsmuchhal@gmail.com>
2024-11-18 23:42:14 +0530

    no changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import './home.css'
 import Programs from '../../components/Programs'
 import Values from '../../components/Values'
 import FAQS from '../../components/FAQS'
+import Testimonials from '../../components/Testimonials'
 
 function Home() {
   return (
@@ -12,6 +13,7 @@ function Home() {
    <Programs/>
    <Values/>
    <FAQS/>
+    <Testimonials/>
    </>
   )
 }
