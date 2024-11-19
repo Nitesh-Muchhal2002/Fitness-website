a37eb9c55c0a76adac6ec74baf73f964664a0794
nitesh <nitsmuchhal@gmail.com>
2024-11-19 17:09:54 +0530

    nothing changed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Programs from '../../components/Programs'
 import Values from '../../components/Values'
 import FAQS from '../../components/FAQS'
 import Testimonials from '../../components/Testimonials'
+import Footer from '../../components/Footer'
 
 function Home() {
   return (
@@ -14,6 +15,7 @@ function Home() {
    <Values/>
    <FAQS/>
     <Testimonials/>
+    <Footer/>
    </>
   )
 }
